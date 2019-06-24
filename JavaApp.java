d577777ae0b31fd211b1ceb46c40dc46fe7befaf
246 <katekraynak@gmail.com>
2019-06-24 16:06:26 -0400

    Update JavaApp.java

--- JavaApp.java
+++ JavaApp.java
@@ -3,43 +3,31 @@ package javaapp;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-//import java.io.FileReader;
-//import java.io.BufferedReader;
-//import java.io.IOException;
+import org.me.mylib.Student;
 
 /**
  *
  * @author kkraynak
+ * 
+ * This program takes a list of student applicants to a program
+ * via a text file and parses through each application, 
+ * eliciting an admissions recommendation to either 
+ * accept, review further, or deny.
+ * 
+ * 
  */
 
-class Student {
-    String firstName;
-    String lastName;
-    String ZNumber;
-    Float gpa;
-    int gmat;
-    int gre;
-    
-    Student(String first, String last, String z, Float gpa, int gmat, int gre){
-        this.firstName=first;
-        this.lastName=last;
-        this.ZNumber=z;
-        this.gpa=gpa;
-        this.gmat=gmat;
-        this.gre=gre;
-    }
-            
-}
+
 public class JavaApp {
-    
+
     Student studentEntry;
-   
+
     public static void main(String[] args) {
         try {
             int i = 0;
             int gmat;
             int gre;
-            String suggestion;
+            
             Student student;
 
             String file = "/Users/kkraynak/Documents/NetBeansProjects/JavaApp/accounting-students/student-list.txt";
@@ -63,23 +51,15 @@ public class JavaApp {
                 } else {
                     gre = Integer.valueOf(greS);
                 }
-                
-                if( (gpa >= 3.5) && ((gmat >= 640) || (gre >= 317)) ){
-                    suggestion = "Accept";
-                }else if( (gpa >= 3.5) || (gmat >= 640) || (gre >=317) || ((gpa >= 3.0) && ((gmat >= 500) || (gre >= 300))) ){
-                    suggestion = "Review further";
-                }else {
-                    suggestion = "Deny";
-                }
-                
-                student = new Student(first, last, zNumber, gpa, gmat, gre);
-                
-                System.out.println("Full name: " + first + " " + last);
-                System.out.println("Z Number: " + zNumber);
-                System.out.println("GPA: " + gpa);
-                System.out.println("GMAT Score: " + gmatS);
-                System.out.println("GRE score: " + greS);
-                System.out.println("Admission suggestion: " + suggestion);
+
+                student = new Student(i, first, last, zNumber, gpa, gmat, gre);
+
+                System.out.println("Full name: " + student.firstName + " " + student.lastName);
+                System.out.println("Z Number: " + student.ZNumber);
+                System.out.println("GPA: " + student.gpa);
+                System.out.println("GMAT Score: " + student.gmat);
+                System.out.println("GRE score: " + student.gre);
+                System.out.println("Admission suggestion: " + student.suggestion);
                 i++;
             }
             scanner.close();
@@ -87,20 +67,4 @@ public class JavaApp {
             e.printStackTrace();
         }
     }
-//        public static void main(String[] args) {
-//        try {
-//            FileReader reader = new FileReader("/Users/kkraynak/Documents/NetBeansProjects/JavaApp/accounting-students/student-list.txt");
-//            BufferedReader bufferedReader = new BufferedReader(reader);
-// 
-//            String line;
-// 
-//            while ((line = bufferedReader.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//            reader.close();
-// 
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
